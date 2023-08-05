fe533455e87b86cb41942fbe26cf84a9290d1f26
Henrique Santos <141459753+Rick1052@users.noreply.github.com>
2023-08-04 22:15:01 -0300

    new commit

--- 2023_02/Solucoes/professor/henrique_santos/problema_01.py
+++ 2023_02/Solucoes/professor/henrique_santos/problema_01.py
@@ -1,1 +1,11 @@
-print("Qualquer coisa")
+import math
+import os
+import random
+import re
+import sys
+
+#para começar o programa
+if __name__ == '__main__'!
+        numero = input()
+        print(numero)
+        
