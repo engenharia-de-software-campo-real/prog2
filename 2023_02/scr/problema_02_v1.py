fa4b92758f1ad3b4e9def8791ebb14181f53939d
Pedro Lealdino Filho <pedro.lealdino@nubank.com.br>
2023-08-24 13:30:30 -0300

    new commit

---
+++ 2023_02/scr/problema_02_v1.py
@@ -0,0 +1,19 @@
+# comeco com importacoes
+import math # Exemplo nao vai servir pra nada no programa
+
+# defino funcoes
+def multiplicacao(numero_a, numero_b):
+    #faco a multiplicacao entre os dois numeros    
+    produto = numero_a * numero_b
+
+    return produto
+
+# chamo a funcao que faz o que eu quero
+a = int(input("a:"))
+b = int(input("b: "))
+mult_01 = multiplicacao(a, b)
+
+print(2 + mult_01)
+
+
+
