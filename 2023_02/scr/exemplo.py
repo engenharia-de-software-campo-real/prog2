caac21032f382b0045cbb2a591eb61bb0f1a8594
Pedro Lealdino Filho <pedro.lealdino@nubank.com.br>
2023-08-24 20:17:36 -0300

    novo commit

---
+++ 2023_02/scr/exemplo.py
@@ -0,0 +1,18 @@
+# imports
+import math 
+
+# escopo do programa
+def main():
+    "programa_inteiro"
+
+
+    funcao_exemplo()
+
+# algumas funcoes
+
+def funcao_exemplo(parametro):
+    "faz_algo"
+
+# incializador do programa
+if __name__ == '__main__':
+    main()
