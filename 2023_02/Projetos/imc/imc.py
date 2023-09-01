28c44b8eab53e30e43ca3a3fc3caa9b2be00c7fe
Pedro Lealdino Filho <pedro.lealdino@nubank.com.br>
2023-08-31 21:56:24 -0300

    novos arquivos

---
+++ 2023_02/Projetos/imc/imc.py
@@ -0,0 +1,4 @@
+# um programa que pede o valor do capital investido
+# o valor dos juros por periodo
+# a quantidade de periodos 
+# e retorna o valor total de rendimento 
---
+++ 2023_02/Projetos/template/template.py
@@ -0,0 +1,12 @@
+#importar bibliotecas
+
+#escopo do programa
+teste = 20
+
+def main():
+
+    print('{}'.format(teste))
+
+
+if __name__ == "__main__":
+    main()
