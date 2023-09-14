49a86f972e717d7f275b461ff1d3c605774211c3
Pedro Lealdino Filho <pedro.lealdino@nubank.com.br>
2023-09-14 13:45:23 -0300

    avaliacao 2 c

---
+++ 2023_02/Avaliacao/turma_c_1b.py
@@ -0,0 +1,35 @@
+'''
+Avaliacao : 1 bimestre - Programacao 2 
+Professor: Pedro Lealdino Filho, PhD.
+
+Integrantes do Grupo: 
+-
+-
+-
+
+Instruções:
+
+0. Faca uma cópia desse arquivo para que você desenvolva sua solução
+1. Após terminar, publique sua solução no classroom na atividade da prova
+
+Problema:
+
+Seu grupo foi contratado para desenvolver um aplicativo para um caixa eletrônico
+de banco.
+
+Seu aplicativo deve conter as seguintes funcionalidades:
+
+- Um menu com opçoes para o usuário
+- Verificar saldo da conta corrente
+- Sacar dinheiro da conta corrente
+- Depositar dinheiro na conta corrente
+- Printar extrato de operacoes de deposito e saque da conta
+
+'''
+
+def main():
+    print("desenvolva aqui ")
+
+
+if __name__ == '__main__':
+    main()
