0a161bd33eb89f6f868346155b8f2664a7bfa840
Pedro Lealdino Filho <pedro.lealdino@nubank.com.br>
2023-09-15 16:46:12 -0300

    edit aval

---
+++ 2023_02/Avaliacao/turma_a_1b.py
@@ -0,0 +1,40 @@
+'''
+Avaliacao : 1 bimestre - Programacao 2 
+Professor: Pedro Lealdino Filho, PhD.
+Turma: A
+
+Integrantes do Grupo: 
+-
+-
+-
+-
+-
+
+Instruções:
+
+0. Faca uma cópia desse arquivo para que você desenvolva sua solução
+1. Após terminar, publique sua solução no classroom na atividade da prova
+
+Problema:
+
+Seu grupo foi contratado para desenvolver um aplicativo gerenciamento de vendas
+e estoque para uma revenda de veículos online. 
+
+Seu aplicativo deve conter as seguintes funcionalidades:
+
+- Um menu com opçoes para o usuário
+- Verificar estoque de veículos: crie um estoque fictício
+- Registrar um veículo
+- Vender um veículo
+- Adicionar o valor de venda do veículo em um controle de finanças, o seu custo e o lucro obtido
+- Printar um demonstrativo de todas as vendas e compras dos veículos 
+
+
+'''
+
+def main():
+    print("desenvolva aqui ")
+
+
+if __name__ == '__main__':
+    main()
